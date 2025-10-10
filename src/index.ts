9211845a742663f103ee14284f7e57ad6c4d21c8
Laurence Summers <lks@live.co.uk>
2025-10-10 15:09:51 +0100

    Fixes type export issue

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 export { calculateSection2 } from "./calculations/section2";
 export { calculateAdditionalHours } from "./calculations/additionalHours";
 export { calculateUsh } from "./calculations/ush";
-export * from "./types/section2";
+export type * from "./types/section2";
